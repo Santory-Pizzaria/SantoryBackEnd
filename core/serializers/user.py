e9705333412d4819198d7bdfd0f1cba3a8263534
gaspatt <matheusgaspar29.if@gmail.com>
2025-09-11 10:53:30 -0300

    feat: Implementação da função de criptografrar a senha do Cadastrar no FrontEnd #1

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -9,3 +9,11 @@ class UserSerializer(ModelSerializer):
         fields = ['id', 'email', 'name', 'password', 'is_active', 'is_staff']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            name=validated_data.get('name', '')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
