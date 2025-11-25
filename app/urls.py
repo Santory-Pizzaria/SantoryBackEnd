91abb6498eda82e4c6b39c5cfc958ffe156501ae
gaspatt <matheusgaspar29.if@gmail.com>
2025-11-25 16:04:28 -0300

    a

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,10 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+
+from uploader.router import router as uploader_router
 
 from core.views import EnderecoViewSet, ItemPedidoViewSet, PedidoViewSet, ProdutoViewSet, UserViewSet, UsuarioViewSet
 from core.views import BebidaViewSet, CarrinhoViewSet, CarrinhoItemViewSet, ComboViewSet, ReservaViewSet, PizzaViewSet
@@ -42,6 +46,9 @@ urlpatterns = [
     ),
     # API
     path('api/', include(router.urls)),
+    path('api/media/', include(uploader_router.urls)), 
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
--- core/models/user.py
+++ core/models/user.py
@@ -44,6 +44,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     passage_id = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('passage_id'), help_text=_('Passage ID'))
     email = models.EmailField(max_length=255, unique=True, verbose_name=_('email'), help_text=_('Email'))
     name = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('name'), help_text=_('Username'))
+    avatar = models.CharField(max_length=512, blank=True, null=True, verbose_name=_('avatar'), help_text=_('URL da imagem do avatar'))
     is_active = models.BooleanField(
         default=True, verbose_name=_('Usuário está ativo'), help_text=_('Indica que este usuário está ativo.')
     )
